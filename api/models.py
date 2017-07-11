115856ad0f8582b4e3ed4666dca35b46e29eafc3
Chiemeka Alim <lord.chiemy@gmail.com>
2017-07-11 16:17:31 +0100

    Generate model via django inspectdb

---
+++ api/models.py
@@ -0,0 +1,85 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey has `on_delete` set to the desired behavior.
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
+from __future__ import unicode_literals
+
+from django.db import models
+
+
+class Address(models.Model):
+    address_id = models.CharField(primary_key=True, max_length=-1)
+    address_line_1 = models.CharField(max_length=-1)
+    address_line_2 = models.CharField(max_length=-1)
+    zip_code = models.CharField(max_length=-1)
+    billing = models.BooleanField()
+
+    class Meta:
+        managed = False
+        db_table = 'address'
+
+
+class Customer(models.Model):
+    customer_id = models.CharField(primary_key=True, max_length=-1)
+    organization_business = models.ForeignKey('OrganizationUnit', models.DO_NOTHING)
+    name = models.CharField(max_length=-1)
+    gender = models.CharField(max_length=-1)
+
+    class Meta:
+        managed = False
+        db_table = 'customer'
+
+
+class CustomerAddress(models.Model):
+    id = models.CharField(primary_key=True, max_length=-1)
+    customer = models.ForeignKey(Customer, models.DO_NOTHING)
+    address = models.ForeignKey(Address, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'customer_address'
+
+
+class OrganizationUnit(models.Model):
+    organization_business_id = models.CharField(primary_key=True, max_length=-1)
+    name = models.CharField(max_length=-1)
+    description = models.CharField(max_length=-1)
+
+    class Meta:
+        managed = False
+        db_table = 'organization_unit'
+
+
+class Service(models.Model):
+    service_id = models.CharField(primary_key=True, max_length=-1)
+    service_type = models.ForeignKey('ServiceType', models.DO_NOTHING)
+    provider = models.ForeignKey(OrganizationUnit, models.DO_NOTHING)
+    description = models.CharField(max_length=-1)
+
+    class Meta:
+        managed = False
+        db_table = 'service'
+
+
+class ServiceLocation(models.Model):
+    id = models.CharField(primary_key=True, max_length=-1)
+    service = models.ForeignKey(Service, models.DO_NOTHING)
+    address = models.ForeignKey(Address, models.DO_NOTHING)
+    latitude = models.IntegerField()
+    longitude = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'service_location'
+
+
+class ServiceType(models.Model):
+    id = models.CharField(primary_key=True, max_length=-1)
+    description = models.CharField(max_length=-1)
+
+    class Meta:
+        managed = False
+        db_table = 'service_type'
