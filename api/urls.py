47c5a3c026f2b08892eaaa94d6838a0b1377e514
Chiemeka Alim <lord.chiemy@gmail.com>
2017-07-12 13:31:58 +0100

    Add swagger for documentation

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,17 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from rest_framework import routers
 from api.views import OrganizationUnitViewSet
 
-router = routers.DefaultRouter()
-router.register(r'organization-units', OrganizationUnitViewSet)
+from rest_framework_swagger.views import get_swagger_view
 
-urlpatterns = router.urls
+router = routers.DefaultRouter()
+router.register(r'org-units', OrganizationUnitViewSet)
+
+docs = get_swagger_view(title='CIS API')
+
+urlpatterns = [
+    url(r'^docs/', docs),
+]
+
+urlpatterns += router.urls
 
