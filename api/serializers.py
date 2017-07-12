5d1a29eac3d49d531fc3065e79bf0b0055c76219
Chiemeka Alim <lord.chiemy@gmail.com>
2017-07-12 13:30:39 +0100

    Generate uuid for organization_business_id

--- api/serializers.py
+++ api/serializers.py
@@ -1,8 +1,18 @@
 from rest_framework import serializers
 from .models import OrganizationUnit
+import uuid
 
 
 class OrganizationUnitSerializer(serializers.ModelSerializer):
+
     class Meta:
         fields = '__all__'
+        extra_kwargs = {
+            'organization_business_id': {'read_only': True}
+        }
         model = OrganizationUnit
+
+    def create(self, validated_data):
+        return OrganizationUnit.objects.create(
+            **validated_data, organization_business_id=uuid.uuid4()
+        )
